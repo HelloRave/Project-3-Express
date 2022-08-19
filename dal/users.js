7d1d38b1b2ebe44048999edebcecd7f43e9bdf61
HelloRave <huang.weiwei@trentglobal.edu.sg>
2022-08-19 04:19:33 +0000

    Added users dal and jwt middleware

---
+++ dal/users.js
@@ -0,0 +1,83 @@
+const crypto = require('crypto')
+const jwt = require('jsonwebtoken')
+const { User } = require('../models')
+
+const getHashedPassword = (password) => {
+    const sha256 = crypto.createHash('sha256')
+    const hash = sha256.update(password).digest('base64')
+    return hash
+}
+
+const generateAccessToken = (user, tokenSecret, time) => {
+    return jwt.sign({
+        user_id: user.get('user_id'),
+        email: user.get('email'),
+        first_name: user.get('first_name'),
+        last_name: user.get('last_name')
+    }, tokenSecret, {
+        expiresIn: time
+    })
+}
+
+const createUser = async (email, password, first_name, last_name) => {
+    const user = await User.where({
+        email: email
+    }).fetch({
+        require: false
+    })
+
+    if (!user) {
+        const newUser = new User({
+            email, first_name, last_name,
+            password: getHashedPassword(password)
+        })
+        await newUser.save()
+        return newUser
+    } else {
+        return false
+    }
+}
+
+const verifyUser = async (email, password) => {
+    const user = await User.where({
+        email: email
+    }).fetch({
+        require: false
+    })
+
+    if (user) {
+        if (user.get('password') === getHashedPassword(password)) {
+            const userSession = {
+                id: user.get('user_id'),
+                email: user.get('email'),
+                firstName: user.get('first_name'),
+                lastName: user.get('last_name')
+            }
+
+            return userSession
+        } else {
+            return false
+        }
+    } else {
+        return false
+    }
+
+}
+
+const getAllUsers = async() => {
+    return await User.fetchAll()
+}
+
+const getUser = async(userId) => {
+    return await User.where({
+        user_id: userId
+    }).fetch({
+        require: true
+    })
+}
+
+module.exports = {
+    getHashedPassword, generateAccessToken,
+    verifyUser, createUser,
+    getAllUsers, getUser
+}
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,37 @@
 const express = require('express')
 const router = express.Router();
-const jwt = require('jsonwebtoken')
-const crypto = require('crypto')
+const jwt = require('jsonwebtoken');
+const { createUser, generateAccessToken } = require('../../dal/users');
+const { checkIfAuthenticatedJWT } = require('../../middlewares');
+
+router.post('/register', async function(req,res){
+    const newUser = await createUser(
+        req.body.email,
+        req.body.password,
+        req.body.first_name,
+        req.body.last_name
+    )
+
+    if (newUser) {
+        const user = await User.where({
+            email: req.body.email
+        }).fetch({
+            require: false
+        })
+        const accessToken = generateAccessToken(user, process.env.TOKEN_SECRET, '1h')
+        res.send(accessToken)
+    } else {
+        res.sendStatus(403)
+    }
+})
+
+router.post('/login', async function(req, res){
+
+})
+
+router.get('/profile', checkIfAuthenticatedJWT, async function(req, res){
+
+})
 
 
 
